1f75c868bbbefae2a614c64a8d5accf9e4f36a67
avesian <aves77@gmail.com>
2026-06-28 22:41:23 +0700

    Create Clipboard.js

---
+++ editor/Clipboard.js
@@ -0,0 +1,47 @@
+```javascript
+/**
+ * Clipboard.js
+ * Internal Clipboard
+ */
+
+export default class Clipboard {
+
+    constructor() {
+        this.data = null;
+        this.type = null;
+    }
+
+    copy(data, type = "text") {
+        this.data = structuredClone(data);
+        this.type = type;
+        return true;
+    }
+
+    cut(data, type = "text") {
+        this.copy(data, type);
+        return this.data;
+    }
+
+    paste() {
+        if (!this.data) return null;
+        return structuredClone(this.data);
+    }
+
+    clear() {
+        this.data = null;
+        this.type = null;
+    }
+
+    hasData() {
+        return this.data !== null;
+    }
+
+    toJSON() {
+        return {
+            type: this.type,
+            hasData: this.hasData()
+        };
+    }
+
+}
+```
