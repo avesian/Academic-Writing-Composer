76b5ca2b08f43b03dc6d5985507c8c31d94ecd64
avesian <aves77@gmail.com>
2026-06-30 23:43:20 +0700

    Implement ParagraphBlock class
    Added ParagraphBlock class extending BaseBlock with alignment and indent properties.

--- blocks/ParagraphBlock.js
+++ blocks/ParagraphBlock.js
@@ -1,1 +1,45 @@
+/**
+ * ParagraphBlock.js
+ */
 
+import BaseBlock from "./BaseBlock.js";
+
+export default class ParagraphBlock extends BaseBlock {
+
+    constructor(data = {}) {
+
+        super(data);
+
+        this.type = "paragraph";
+
+        this.alignment = data.alignment || "justify";
+
+        this.indent = data.indent ?? true;
+
+    }
+
+    render() {
+
+        return `
+            <p class="awc-paragraph">
+                ${this.content}
+            </p>
+        `;
+
+    }
+
+    toJSON() {
+
+        return {
+
+            ...super.toJSON(),
+
+            alignment: this.alignment,
+
+            indent: this.indent
+
+        };
+
+    }
+
+}
