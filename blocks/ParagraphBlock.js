d30a68dac90ffaef06f02d4bc8923cefa28e6e22
avesian <aves77@gmail.com>
2026-07-01 00:42:56 +0700

    Refactor ParagraphBlock class
    Refactor ParagraphBlock to simplify render method and remove unnecessary constructor properties.

--- blocks/ParagraphBlock.js
+++ blocks/ParagraphBlock.js
@@ -1,45 +1,9 @@
-/**
- * ParagraphBlock.js
- */
+render() {
 
-import BaseBlock from "./BaseBlock.js";
-
-export default class ParagraphBlock extends BaseBlock {
-
-    constructor(data = {}) {
-
-        super(data);
-
-        this.type = "paragraph";
-
-        this.alignment = data.alignment || "justify";
-
-        this.indent = data.indent ?? true;
-
-    }
-
-    render() {
-
-        return `
-            <p class="awc-paragraph">
-                ${this.content}
-            </p>
-        `;
-
-    }
-
-    toJSON() {
-
-        return {
-
-            ...super.toJSON(),
-
-            alignment: this.alignment,
-
-            indent: this.indent
-
-        };
-
-    }
+    return `
+        <p class="awc-paragraph">
+            [Paragraph]
+        </p>
+    );
 
 }
