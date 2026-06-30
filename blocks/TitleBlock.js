22af126f7c7aad0cce3a1f291a1e702c3a402978
avesian <aves77@gmail.com>
2026-06-30 23:46:56 +0700

    Implement TitleBlock class with subtitle feature
    Added TitleBlock class extending BaseBlock with subtitle support.

--- blocks/TitleBlock.js
+++ blocks/TitleBlock.js
@@ -1,1 +1,51 @@
+/**
+ * TitleBlock.js
+ */
 
+import BaseBlock from "./BaseBlock.js";
+
+export default class TitleBlock extends BaseBlock {
+
+    constructor(data = {}) {
+
+        super(data);
+
+        this.type = "title";
+
+        this.subtitle = data.subtitle || "";
+
+    }
+
+    render() {
+
+        return `
+            <header class="awc-title-page">
+
+                <h1 class="awc-title">
+                    ${this.content}
+                </h1>
+
+                ${
+                    this.subtitle
+                        ? `<h2 class="awc-subtitle">${this.subtitle}</h2>`
+                        : ""
+                }
+
+            </header>
+        `;
+
+    }
+
+    toJSON() {
+
+        return {
+
+            ...super.toJSON(),
+
+            subtitle: this.subtitle
+
+        };
+
+    }
+
+}
