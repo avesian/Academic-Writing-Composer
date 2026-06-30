24b97f7f2976f2358ea44339bf81cda739107b71
avesian <aves77@gmail.com>
2026-06-30 23:44:44 +0700

    Add HeadingBlock class for rendering headings

--- blocks/HeadingBlock.js
+++ blocks/HeadingBlock.js
@@ -1,1 +1,47 @@
+/**
+ * HeadingBlock.js
+ */
 
+import BaseBlock from "./BaseBlock.js";
+
+export default class HeadingBlock extends BaseBlock {
+
+    constructor(data = {}) {
+
+        super(data);
+
+        this.type = "heading";
+
+        this.level = data.level || 1;
+
+        this.numbering = data.numbering ?? true;
+
+    }
+
+    render() {
+
+        const tag = `h${Math.min(this.level, 6)}`;
+
+        return `
+            <${tag} class="awc-heading awc-heading-${this.level}">
+                ${this.content}
+            </${tag}>
+        `;
+
+    }
+
+    toJSON() {
+
+        return {
+
+            ...super.toJSON(),
+
+            level: this.level,
+
+            numbering: this.numbering
+
+        };
+
+    }
+
+}
