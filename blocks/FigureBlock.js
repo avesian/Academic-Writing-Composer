0a421fae1be5bf260b0f3678596df742df7918e3
avesian <aves77@gmail.com>
2026-06-30 23:51:22 +0700

    Add FigureBlock class for rendering figures

--- blocks/FigureBlock.js
+++ blocks/FigureBlock.js
@@ -1,1 +1,50 @@
+/**
+ * FigureBlock.js
+ */
 
+import BaseBlock from "./BaseBlock.js";
+
+export default class FigureBlock extends BaseBlock {
+
+    constructor(data = {}) {
+
+        super(data);
+
+        this.type = "figure";
+
+        this.src = data.src || "";
+
+        this.caption = data.caption || "";
+
+        this.width = data.width || "100%";
+
+    }
+
+    render() {
+
+        return (
+            '<figure class="awc-figure">' +
+                '<img src="' + this.src + '" alt="' + this.caption + '" style="width:' + this.width + ';">' +
+                '<figcaption>' + this.caption + '</figcaption>' +
+            '</figure>'
+        );
+
+    }
+
+    toJSON() {
+
+        return {
+
+            ...super.toJSON(),
+
+            src: this.src,
+
+            caption: this.caption,
+
+            width: this.width
+
+        };
+
+    }
+
+}
