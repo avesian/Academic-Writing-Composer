d754b95f9d6f8cd065a43c7fcd59c59bac7c806f
avesian <aves77@gmail.com>
2026-06-30 15:16:12 +0700

    Refactor BlockFactory class and add registration methods
    Refactor BlockFactory to use Map for block types and added register/unregister methods.

--- js/BlockFactory.js
+++ js/BlockFactory.js
@@ -1,44 +1,93 @@
+```javascript
 /**
  * BlockFactory.js
+ * Academic Writing Composer
  */
 
-export default class BlockFactory {
-    constructor() {
-        // Daftar tipe blok yang didukung
-        this.types = {
-            paragraph: ParagraphBlock
-        };
-    }
-
-    create(type, data = {}) {
-        const BlockClass = this.types[type];
-        
-        if (!BlockClass) {
-            console.warn(`Block type "${type}" tidak ditemukan.`);
-            return null;
-        }
-        
-        return new BlockClass(data);
-    }
-}
-
-// Class dasar untuk Paragraf agar tidak error saat dipanggil
 class ParagraphBlock {
+
     constructor(data = {}) {
-        this.id = data.id || "block-" + Math.random().toString(36).substr(2, 9);
+
+        this.id =
+            data.id ??
+            crypto.randomUUID();
+
         this.type = "paragraph";
-        this.content = data.content || "";
+
+        this.content =
+            data.content ?? "";
+
     }
 
     render() {
+
         return `<p>${this.content}</p>`;
+
     }
 
     toJSON() {
+
         return {
+
             id: this.id,
+
             type: this.type,
+
             content: this.content
+
         };
+
     }
+
 }
+
+export default class BlockFactory {
+
+    constructor() {
+
+        this.types = new Map();
+
+        this.register(
+            "paragraph",
+            ParagraphBlock
+        );
+
+    }
+
+    register(type, BlockClass) {
+
+        this.types.set(type, BlockClass);
+
+    }
+
+    unregister(type) {
+
+        this.types.delete(type);
+
+    }
+
+    has(type) {
+
+        return this.types.has(type);
+
+    }
+
+    create(type, data = {}) {
+
+        const BlockClass =
+            this.types.get(type);
+
+        if (!BlockClass) {
+
+            throw new Error(
+                `Unknown block type: ${type}`
+            );
+
+        }
+
+        return new BlockClass(data);
+
+    }
+
+}
+```
