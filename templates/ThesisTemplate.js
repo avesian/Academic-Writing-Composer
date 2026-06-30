6e6a013677406981b34a54503dab15308993341c
avesian <aves77@gmail.com>
2026-07-01 00:18:02 +0700

    Add ThesisTemplate for academic writing structure
    Created a ThesisTemplate class extending DocumentTemplate to structure academic writing.

---
+++ templates/ThesisTemplate.js
@@ -0,0 +1,64 @@
+/**
+ * ThesisTemplate.js
+ * Academic Writing Composer
+ */
+
+import DocumentTemplate from "../DocumentTemplate.js";
+
+export default class ThesisTemplate extends DocumentTemplate {
+
+    constructor() {
+
+        super("thesis");
+
+        this
+            .add("title")
+            .add("chapter", {
+                number: 1,
+                title: "PENDAHULUAN"
+            })
+            .add("heading", {
+                level: 2,
+                content: "Latar Belakang"
+            })
+            .add("paragraph")
+
+            .add("heading", {
+                level: 2,
+                content: "Rumusan Masalah"
+            })
+            .add("paragraph")
+
+            .add("heading", {
+                level: 2,
+                content: "Tujuan Penelitian"
+            })
+            .add("paragraph")
+
+            .add("chapter", {
+                number: 2,
+                title: "TINJAUAN PUSTAKA"
+            })
+            .add("paragraph")
+
+            .add("chapter", {
+                number: 3,
+                title: "METODE PENELITIAN"
+            })
+            .add("paragraph")
+
+            .add("chapter", {
+                number: 4,
+                title: "HASIL DAN PEMBAHASAN"
+            })
+            .add("paragraph")
+
+            .add("chapter", {
+                number: 5,
+                title: "PENUTUP"
+            })
+            .add("paragraph");
+
+    }
+
+}
