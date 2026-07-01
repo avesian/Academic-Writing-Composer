13c5319ca5cd166f5c6ae207ef81097101b68b9a
avesian <aves77@gmail.com>
2026-07-02 00:05:28 +0700

    Refactor Canvas class and integrate DocumentEditor

--- ui/Canvas.js
+++ ui/Canvas.js
@@ -1,17 +1,136 @@
-this.app.on("block:add", type => {
+/**
+ * Canvas.js
+ * Academic Writing Composer
+ */
 
-    console.log("ADD BLOCK:", type);
+import DocumentEditor from "../editor/DocumentEditor.js";
 
-    const block = this.documentEditor.addBlock(type);
+export default class Canvas {
 
-    console.log(block);
+    constructor(app) {
 
-    console.log(
-        this.documentEditor
-            .getDocument()
-            .getBlocks()
-    );
+        this.app = app;
 
-    this.refresh();
+        this.element = null;
 
-});
+        this.page = null;
+
+        this.editor = null;
+
+        this.documentEditor = null;
+
+    }
+
+    render() {
+
+        console.log("Canvas.render()");
+
+        if (this.element) {
+
+            return this.element;
+
+        }
+
+        this.element = document.createElement("section");
+
+        this.element.className = "awc-canvas";
+
+        this.page = document.createElement("div");
+
+        this.page.className = "awc-page";
+
+        this.editor = document.createElement("div");
+
+        this.editor.className = "awc-editor";
+
+        this.editor.contentEditable = true;
+
+        this.page.appendChild(this.editor);
+
+        this.element.appendChild(this.page);
+
+        this.documentEditor = new DocumentEditor(this.app);
+
+        console.log("DocumentEditor created");
+
+        this.bindEvents();
+
+        this.documentEditor.newDocument();
+
+        this.refresh();
+
+        return this.element;
+
+    }
+
+    bindEvents() {
+
+        console.log("Canvas.bindEvents()");
+
+        this.editor.addEventListener("input", () => {
+
+            this.documentEditor.setContent(
+                this.editor.innerHTML
+            );
+
+        });
+
+        this.app.on("block:add", type => {
+
+            console.log("BLOCK EVENT:", type);
+
+            const block = this.documentEditor.addBlock(type);
+
+            console.log("BLOCK =", block);
+
+            console.log(
+                "TOTAL BLOCKS =",
+                this.documentEditor
+                    .getDocument()
+                    .getBlocks()
+                    .length
+            );
+
+            this.refresh();
+
+        });
+
+        this.app.on("document:new", () => {
+
+            console.log("DOCUMENT NEW");
+
+            this.documentEditor.newDocument();
+
+            this.refresh();
+
+        });
+
+        this.app.on("document:refresh", () => {
+
+            console.log("DOCUMENT REFRESH");
+
+            this.refresh();
+
+        });
+
+    }
+
+    refresh() {
+
+        console.log("Canvas.refresh()");
+
+        const html = this.documentEditor.renderHTML();
+
+        console.log(html);
+
+        this.editor.innerHTML = html;
+
+    }
+
+    getEditor() {
+
+        return this.documentEditor;
+
+    }
+
+}
