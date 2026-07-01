e26d7ff4ae25032294e82cb0fdff0d8cd457cd67
avesian <aves77@gmail.com>
2026-07-02 01:20:42 +0700

    Fix import path for Block in AbstractEnglishBlock

--- js/AbstractEnglishBlock.js
+++ js/AbstractEnglishBlock.js
@@ -4,7 +4,7 @@
  * Version: 1.0.0
  */
 
-import Block from "./block/Block.js";
+import Block from "./js/Block.js";
 
 export default class AbstractEnglishBlock extends Block {
     constructor(options = {}) {
